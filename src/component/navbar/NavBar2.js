a559f534b354ad418dba2afecd53a6543285c82c
Lam Duc Phat <lamducphat5665@gmail.com>
2024-08-06 05:19:24 +0700

    6/8/2024

--- src/component/navbar/NavBar2.js
+++ src/component/navbar/NavBar2.js
@@ -1,31 +1,60 @@
-import React from 'react'
-// import './NavBar2.css'
+
+import React, {useState}from 'react'
+import './NavBar2.css'
 import { Navbar, Nav, Container, NavItem, NavLink } from 'reactstrap'
+import {Link } from 'react-router-dom'
+import { Button } from '../Button'
+import { CiMenuBurger } from 'react-icons/ci'
+import logo from './logo.png'
+import { IoMdClose } from 'react-icons/io'
 export default function NavBar2() {
+    const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true)
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
+    const showButton = () =>{
+        if (window.innerWidth <=960){
+            setButton(false)
+        }else{
+            setButton(true)
+        }
+    }
+    window.addEventListener('resize', showButton)
   return (
     <div>
-        <div className='container-fluid '>
-            <Navbar color="light" light expand="md" >
-                <Nav className="w-100">
-                    <NavItem>
-                        <NavLink href="/">Home</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/">Home</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/">Home</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/">Home</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/">Home</NavLink>
-                    </NavItem>
-                </Nav>
-            </Navbar>
+        <div className='navbar '>
+            <div className='navbar-container'>
+                <Link to='/' classname='navbar-logo'>
+                    logo <img src={logo} width={20}/>
+                </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click?'fa-solid fa-xmark':'fa-solid fa-bars'}></i>
+                    </div>
+                    <ul className={click?'nav-menu active':'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' classname='navbar-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/' classname='navbar-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/' classname='navbar-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/' classname='navbar-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        </ul>
+                        {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
          </div>
-       
+       </div>
     </div>
   )
 }
